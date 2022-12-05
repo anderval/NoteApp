7073955274f3f631d5f5f54d2d179724e11289d0
Anderval <andervalsp@outlook.com>
2022-12-05 12:44:27 -0300

    Implemented POST and Flash Messaging!

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
@@ -13,6 +15,23 @@ def logout():
     return render_template("home.html")
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passowrds don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash(' Password must be at least 7 characters.', category='error')
+        else:
+            flash('Acoount created!', category='success')
+
     return render_template("sign_up.html")
