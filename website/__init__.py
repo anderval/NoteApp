495aeee2c286dc01c1846f865e48912ef0aa6189
Anderval <andervalsp@outlook.com>
2022-12-07 17:28:23 -0300

    SQLAlchemy changes

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -17,4 +19,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
     return app
+
+
+
